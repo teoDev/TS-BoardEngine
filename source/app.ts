280e850aa177c7390f9f2924240a574afa45c959
kwozniak <k.wozniak3@tt.com.pl>
2017-02-08 22:31:47 +0100

    Connection by socket.IO working

---
+++ source/app.ts
@@ -0,0 +1,68 @@
+﻿
+import express = require("express");
+import socketio = require("socket.io");
+import http = require("http");
+import Deck from "./entities/Deck";
+
+
+let app = express();
+app.set("port", 8080);
+
+
+let server = http.createServer(app);
+let io = socketio(server);
+server.listen(app.get("port"), function () {
+  console.log("Express server listening on port " + app.get("port"));
+});
+
+let counter = 0;
+
+class GameServer {
+  private tanks = [];
+  private decks = Array<Deck>();
+
+  public addPlayer(tank) {
+    this.tanks.push(tank);
+  }
+  public addDeck(ball: Deck) {
+    this.decks.push(ball);
+  }
+ public removeTank(tankId) {
+    this.tanks = this.tanks.filter(function (t) { return t.id !== tankId; });
+  }
+  public sync() {
+    console.log("sync");
+  }
+}
+
+
+let game = new GameServer();
+
+io.on("connection", function (client) {
+  console.log("User connected");
+
+  client.on("joinGame", function (tank) {
+    console.log(tank.id + " joined the game");
+    game.addPlayer({ id: tank.id, type: tank.type, hp: 2 });
+  });
+
+  client.on("sync", function (data) {
+    // Receive data from clients
+    if (data.tank !== undefined) {
+      game.sync();
+    }
+
+    counter++;
+  });
+
+  client.on("pickCard", function () {
+    console.log("pickCard");
+  });
+
+  client.on("leaveGame", function (tankId) {
+    console.log(tankId + " has left the game");
+    game.removeTank(tankId);
+    client.broadcast.emit("removeTank", tankId);
+  });
+
+});
--- source/main.ts
+++ source/main.ts
@@ -2,10 +2,32 @@ import * as $ from "jquery";
 import Greeter from "./entities/greeter";
 import StandardDeck from "./entities/StandardDeck";
 import Board from "./entities/Board";
-
-
+import * as socketIo from "socket.io-client";
 let canvas: HTMLCanvasElement;
 let ctx: CanvasRenderingContext2D;
+
+class SocketService {
+     private _socket;
+     private SERVER_URL = "http://localhost:8080";
+
+     public getSocket() {
+      if (this._socket === undefined) {
+        this.initSocket();
+      }
+      return this._socket;
+    }
+
+    private initSocket(): void {
+        this._socket = socketIo(this.SERVER_URL);
+    }
+
+}
+
+
+
+
+let socketService = new SocketService();
+let io = socketService.getSocket();
 
 function gameLoop() {
    requestAnimationFrame(gameLoop);
@@ -19,7 +41,19 @@ $( document ).ready(function() {
    let deck = new StandardDeck();
    board.draw(deck.getRandomCard(), 15, 15);
    board.draw(deck, 100, 100);
+
+
+    $("#getCard").click(function () {
+     joinGame(io);
+  });
 });
+
+function joinGame(socket) {
+  if (socket !== undefined) {
+    socket.emit("joinGame", { id: "12" });
+  }
+}
+
 
 
 let greeter = new Greeter("world!");
